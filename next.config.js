2cb49cb4c4081279bdf16931169a74c7c9c78e0a
Kostya <kimkonstantin2118@gmail.com>
2026-06-17 23:46:26 +0300

    Don

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
     formats: ['image/avif', 'image/webp'],
   },
   compress: true,
-  swcMinify: true,
   reactStrictMode: true,
 };
 
